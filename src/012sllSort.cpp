ee63ee064f7a695cb362d7460d4e3c44241d70e1
monikaavalur <monika.avalur@gmail.com>
2015-11-22 13:21:40 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -20,7 +20,34 @@ struct node {
 	int data;
 	struct node *next;
 };
-
-void sll_012_sort(struct node *head){
-	
+void swap(struct node *Arr,struct node *Arr1);
+void sll_012_sort(struct node *head)
+{
+	int index1;
+	struct node *ptr;
+	ptr = head;
+	struct node *ptr1 = NULL;
+	if (ptr == NULL)
+		return;
+	do
+	{
+		index1 = 0;
+		ptr = head;
+		while (ptr->next != ptr1)
+		{
+			if (ptr->data > ptr->next->data)
+			{
+				swap(ptr, ptr->next);
+				index1 = 1;
+			}
+			ptr = ptr->next;
+		}
+		ptr1 = ptr;
+	} while (index1);
+}
+void swap(struct node *Arr, struct node *Arr1)
+{
+	int temp = Arr->data;
+	Arr->data = Arr1->data;
+	Arr1->data = temp;
 }
